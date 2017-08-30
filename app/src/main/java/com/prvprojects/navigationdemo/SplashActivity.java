ef7c4cdf6a20bed97f0e5d4735eb1166e22eb350
Pankaj <pankajkprojects@gmail.com>
2017-08-31 01:41:05 +0530

    DONE - Demo Google Maps Activity added and working SMALL CHANGE - Put is launcher background color to color file
    NEXT - Add an abstract class BaseNavigationActivity which will be extended by NavigationActivity and implement functionalities keeping coding manageable.
    
    BENEFIT - We can easily update methods in Base Activity without need to update actual NavigationActivity implementation.

--- app/src/main/java/com/prvprojects/navigationdemo/SplashActivity.java
+++ app/src/main/java/com/prvprojects/navigationdemo/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(SplashActivity.this, DummyActivity.class);
+                Intent intent = new Intent(SplashActivity.this, DemoMapActivity.class);
                 startActivity(intent);
                 finish();
 
